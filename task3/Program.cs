d9a180469dc0cd9c6940444a1b6299da8a362c04
LeraSidarava <126901447+LeraSidarava@users.noreply.github.com>
2023-04-20 20:15:43 +0200

    Add files via upload
    add functionality task 3 lecture 4

---
+++ task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int a = 0;
+        int b = 1;
+
+        Console.Write(a);
+
+        while (b <= n)
+        {
+            Console.Write(" " + b);
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        Console.WriteLine();
+    }
+}
